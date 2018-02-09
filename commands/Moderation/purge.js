8dd9df2cc1b7d99f750fc5cc9c894186010e7893
Glitch (goombah) <none>
2018-02-09 23:05:43 +0000

    backup ^^

--- commands/Moderation/purge.js
+++ commands/Moderation/purge.js
@@ -1,39 +1,25 @@
-const Discord = require("discord.js")
-exports.run = async (client, message, args) => {
-    await message.delete();
-    const user = message.mentions.users.first();
-    const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-    if (!amount) return message.channel.send('❎ | Sorry, but you must specify an amount to delete!');
-    if (!amount && !user) return message.channel.send('❎ | Sorry, but you must specify a user and amount, or just an amount, of messages to purge!');
-    if (amount >= 101) return message.channel.send('❎ | Sorry, but you cannot purge more than 100 messages.');
-    message.channel.messages.fetch({
-     limit: amount,
-    }).then((messages) => {
-     if (user) {
-     const filterBy = user ? user.id : client.user.id;
-     messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-     }
-     message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-    });
-    message.channel.send(`Correctly purged ${args[0]} messages`).then(m => setTimeout(async () => {m.delete()}), 1000)
+exports.run = async (client, msg, [user = client.user, amount]) => {
+  let messages = await msg.channel.messages.fetch({ limit: amount });
+  messages = messages.filter(m => m.author.id === user.id);
+  if (client.config.selfbot) return messages.forEach(m => m.delete().catch((e) => { throw new Error(e); }));
+  return msg.channel.bulkDelete(messages);
 };
 
 exports.conf = {
-   enabled: true,
-   runIn: ["text", "group"],
-   aliases: ["prune"],
-   permLevel: 2,
-   botPerms: [],
-   requiredFuncs: [],
-   requiredSettings: [],
- };
+  enabled: true,
+  runIn: ["text", "dm", "group"],
+  selfbot: false,
+  aliases: [],
+  permLevel: 0,
+  botPerms: ["MANAGE_MESSAGES"],
+  requiredFuncs: [],
+  requiredModules: [],
+};
 
 exports.help = {
   name: 'purge',
   description: 'Purges (deletes) a given number of messages, excluding the command itself.',
-  usage: '[messages:int|user:user] <messages:int>',
+  usage: "[user:mention] <amount:int{2,100}>",
   usageDelim: " ",
-  extendedHelp: `To use it, you must do the following:
--messages Deletes the given no. of messages
--@user messages Deletes the given no. of messages by the given user`
+  type: "commands",
 };
