530108cfd4cab6cf51f0c846df97ac6e4b5d8c68
Glitch (efficacious-snowman) <none>
2018-02-19 20:44:57 +0000

    👦💒 Updated with Glitch

--- commands/Fun/kill.js
+++ commands/Fun/kill.js
@@ -1,19 +1,19 @@
-exports.run = async (client, message, mention) => {
+exports.run = async (client, message, [mention]) => {
     Array.prototype.random = function() {
       return this[Math.floor(Math.random() * this.length)]
     };
-    if (message.mentions.members.size === 0) return message.reply("please mention a user to kill");
+    // if (message.mentions.members.size === 0) return message.reply("please mention a user to kill");
     let killed;
-    if (message.mentions.members.first().id == client.user.id) {
+    if (mention.id == client.user.id) {
       killed = ["me", "I'm", "I", "me"]
     } else {
-      const killVerb = message.mentions.members.first() + "'s";
-      killed = [message.mentions.members.first(), killVerb, "he", "him"]
+      const killVerb = mention + "'s";
+      killed = [mention, killVerb, "he", "him"]
     }
     var arms = ["a 🔫 **gun**", "a 🍌 **banana**","a 📌 **pin**", "a 🎷 **sax**", "👊 **his hands**", "a 💣 **bomb**"];
     var killMethods = [`${message.author} killed ${killed[0]} with ${arms.random()}!`, `Turns out that ${killed[1]} dead because ${message.author} killed ${killed[3]} with ${arms.random()}!`];
     var suicideMethods = [`${message.author} killed himself with ${arms.random()}!`]
-    if (message.mentions.members.first().id == message.author.id) {
+    if (mention.id == message.author.id) {
       message.channel.send(`❎ | Do you really want to suicide? Ok then... ${suicideMethods.random()}!`).catch(console.error);
     } else {
       message.channel.send(`✅ | ${killMethods.random()}!`).catch(console.error);
@@ -33,6 +33,6 @@ exports.conf = {
 exports.help = {
    name: "kill",
    description: "Kills someone.",
-   usage: "[someone:mention]",
+   usage: "[someone:user]",
    usageDelim: "",
  };
